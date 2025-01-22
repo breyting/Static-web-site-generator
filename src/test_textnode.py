f45c00c8f3a4e25d3b5b4660c86178555191ee55
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-01-22 13:57:14 +0100

    add the text_node_to_html_node func

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
+from htmlnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -12,6 +13,37 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
         self.assertNotEqual(node, node3)
         self.assertNotEqual(node3, node4)
+
+    def test_text_node_to_html_node(self):
+        text_node_normal = TextNode("blabla", TextType.NORMAL)
+        html_node = text_node_to_html_node(text_node_normal)
+        result_text_node_normal_to_html_node = LeafNode(None, "blabla")
+        self.assertEqual(
+            text_node_to_html_node(text_node_normal), 
+            result_text_node_normal_to_html_node
+        )
+
+        text_node_link = TextNode("blabla", TextType.LINK, "haha.com")
+        result_text_node_link_to_html_node = LeafNode("a", "blabla", {"href" : "haha.com"})
+        self.assertEqual(
+            text_node_to_html_node(text_node_link), 
+            result_text_node_link_to_html_node
+        )
+
+        text_node_wrong = TextNode("blibli", "hihi", "huhu.com")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(text_node_wrong)
+    
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import *
 
 class TextType(Enum):
     NORMAL = "normal"
@@ -9,7 +10,7 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
-    def __init__(self, text, text_type: TextType, url=None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -23,4 +24,24 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href" : text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {
+                "src" : text_node.url,
+                "alt" : text_node.text
+            })
+        case _:
+            raise ValueError("Text type not allowed")
