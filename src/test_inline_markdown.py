2d609e55491372721081ca64eb37134cefc08663
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-01-23 19:59:50 +0100

    added the boot.dev test and it works perfectly !

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -142,3 +142,21 @@ class Test_extract_markdown(unittest.TestCase):
             extract_markdown_links(text),
             [("to boot dev", "https://www.boot.dev")]
         )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
